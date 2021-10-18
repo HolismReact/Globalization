a6a0c959b704968ae76e63d1f5014c9d4d7fd1d4
Saeed Nemati <sn.1361@gmail.com>
2021-10-18 10:09:09 +0330

    committed by script

--- Admin/Locales/List.js
+++ Admin/Locales/List.js
@@ -2,17 +2,23 @@ import { List, Text, ItemAction, app, post } from '@List';
 
 const headers = <>
     <th>Key</th>
+    <th>Local Key</th>
+    <th>Is Active</th>
+    <th>Country</th>
 </>
 
 const row = (item) => {
     return <>
         <td>{item.key}</td>
+        <td>{item.localKey}</td>
+        <td>{item.isActive}</td>
+        <td>{item.country}</td>
     </>
 }
 
 const Locales = () => {
     return <List
-        title="Locales 6"
+        title="Locales"
         entity='locale'
         headers={headers}
         row={row}
