592242a8c98bd013f958516391e61b29e5ee0a6b
Saeed Nemati <sn.1361@gmail.com>
2021-10-20 13:38:58 +0330

    committed by script

--- User/Choose.js
+++ User/Choose.js
@@ -13,21 +13,33 @@ const Locales = () => {
         get('/locale/actives')
             .then(data => {
                 setProgress(false);
-                setLocales(locales);
+                setLocales(data);
             }, error => {
                 setProgress(false);
                 app.error(error);
             })
     }, []);
 
-    return <div className="rounded-md border w-56 h-56 flex justify-center items-center bg-white">
+    return <div className="rounded-md border w-56 flex flex-col justify-center items-center bg-white py-4">
 
         {
             progress
                 ?
                 <CircularProgress />
                 :
-                locales.map(locale => <div>{locale.localeKey}</div>)
+                locales.map((locale, index) => <div
+                    key={index}
+                    className={
+                        "w-full flex py-2 justify-center items-center hover:bg-green-200 cursor-pointer " +
+                        ((index !== locales.length - 1) ? "mb-2" : "")
+                    }
+                    onClick={() => {
+                        document.cookie = 'culture=' + locale.key;
+                        document.location.reload();
+                    }}
+                >
+                    {locale.localKey}
+                </div>)
         }
 
     </div>
