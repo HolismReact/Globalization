3ffa24cbcbabb8e6e50b848bb40692c0384ee47b
Saeed Nemati <sn.1361@gmail.com>
2022-05-28 11:42:33 +0430

    committed by script

---
+++ Admin/Exports.js
@@ -0,0 +1,36 @@
+import TranslateIcon from '@mui/icons-material/Translate';
+import Locales from './Locale/List'
+import Translations from './Translation/List'
+
+const GlobalizationRoutes = [
+    {
+        path: "/locales",
+        component: Locales
+    },
+    {
+        path: "/translations",
+        component: Translations
+    }
+]
+
+const GlobalizationMenu = [
+    {
+        title: "Globalization",
+        icon: TranslateIcon,
+        children: [
+            {
+                title: "Locales",
+                url: "/locales"
+            },
+            {
+                title: "Translations",
+                url: "/translations"
+            }
+        ]
+    }
+]
+
+export { GlobalizationRoutes }
+export { GlobalizationMenu }
+export { Locales }
+export { Translations }
